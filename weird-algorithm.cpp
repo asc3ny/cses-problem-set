11d8490e14ddc4b2d7537bddd01156573d0730e6
ep1ctet <58085779+ep1ctet@users.noreply.github.com>
2021-11-03 16:25:50 +0300

    Create weird-algorithm.cpp

---
+++ weird-algorithm.cpp
@@ -0,0 +1,24 @@
+// CSES: Weird Algorithm
+ 
+#include <iostream>
+using namespace std;
+ 
+int main() 
+{
+    long int inputNumber;
+    cin >> inputNumber;
+ 
+    while (true)
+    {
+        cout << inputNumber << " ";
+ 
+        if (inputNumber == 1) {
+            break;
+        } else if (inputNumber % 2 == 0) {
+            inputNumber = inputNumber / 2;
+        } else {
+            inputNumber = (inputNumber * 3) + 1;
+        }
+    }
+    return 0;
+}
