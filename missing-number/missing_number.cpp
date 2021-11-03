7c1aa2ab95caaba72aafd40c8f69deedafc3d0bf
ep1ctet <roshick14@gmail.com>
2021-11-03 20:18:45 +0300

    Change files and dirs names, add new cses problem

---
+++ missing-number/missing_number.cpp
@@ -0,0 +1,19 @@
+// CSES: Missing Number
+// In progress
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+int main() 
+{
+    string inputValue;
+    
+    getline(cin, inputValue);
+
+    cout << inputValue;
+
+    return 0;
+}
--- weird-algorithm/weird_algorithm.cpp
+++ weird-algorithm/weird_algorithm.cpp
@@ -2,16 +2,16 @@
  
 #include <iostream>
 using namespace std;
- 
+
 int main() 
 {
     long int inputNumber;
     cin >> inputNumber;
- 
+
     while (true)
     {
         cout << inputNumber << " ";
- 
+
         if (inputNumber == 1) {
             break;
         } else if (inputNumber % 2 == 0) {
