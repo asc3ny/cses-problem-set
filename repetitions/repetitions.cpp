39cf49cdca1f000b1440e3cf984381b247b96df6
ep1ctet <58085779+ep1ctet@users.noreply.github.com>
2021-11-05 01:20:41 +0300

    First code

--- repetitions/repetitions.cpp
+++ repetitions/repetitions.cpp
@@ -1,3 +1,17 @@
 // CSES: Repetitions
 // Status: IN PROGRESS
 // Time: 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    string inputString;
+    cin >> inputString;
+
+    for (int i = 0; i < inputString.length(); ++i)
+    {
+        cout << inputString[i] << ' ';
+    }
+}
