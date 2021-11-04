8de12971626182b2c7ecd2d8dead15b82ed4f205
ep1ctet <58085779+ep1ctet@users.noreply.github.com>
2021-11-04 19:17:39 +0300

    New try find a solution

--- missing-number/missing_number.cpp
+++ missing-number/missing_number.cpp
@@ -3,27 +3,25 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main()
 {
     int maxLength;
     cin >> maxLength;
-
+ 
     int listNumbers[maxLength];
     for (int i = 0; i < maxLength-1; ++i)
     {
         cin >> listNumbers[i];
     }
-
+ 
     sort(listNumbers, listNumbers + maxLength);
     for (int i = 1; i < maxLength; ++i)
     {
-        cout << i << "<=" << listNumbers[i] << endl;
-        // if (listNumbers[i] != i)
-        // {
-        //     cout << listNumbers[i] << endl;
-        // }
+        if (listNumbers[i] != i)
+        {
+            cout << i << endl;
+        }
     }
-
     return 0;
 }
