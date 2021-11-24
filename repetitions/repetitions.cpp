b021dad931b75d3e6dcea5e3c6400b4e22207863
Evgeny Vasiliev <58085779+ep1ctet@users.noreply.github.com>
2021-11-24 10:43:46 +0300

    Create repetitions.cpp

---
+++ repetitions/repetitions.cpp
@@ -0,0 +1,50 @@
+/* 
+ * CSES: Repetitions
+ * Status: READY
+ * Time: 0.05 s
+ * Characters: 411
+ * TODO: Warning on compiler
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector <char> chrLst;
+    char usrInp;
+    char lastChar;
+    int globalCount = 0;
+    int localCount = 0;
+
+    while (cin >> usrInp) {
+        chrLst.push_back(usrInp);
+    }
+
+    for (char chr : chrLst) {
+        
+        if (!lastChar) {
+            lastChar = chr;
+            localCount = 1;
+            continue;
+        }
+
+        if (lastChar == chr) {
+            localCount++;
+            lastChar = chr;
+        }
+
+        if (lastChar != chr) {
+            localCount = 1;
+            lastChar = chr;
+        }
+
+        if (localCount > globalCount) {
+            globalCount = localCount;
+        }
+    }
+
+    cout << globalCount;
+
+    return 0;
+}
