5b7f248b144a174aa9cdf1f21d70f2e2a67404d2
Evgeny Vasiliev <58085779+ep1ctet@users.noreply.github.com>
2021-11-26 17:17:14 +0300

    Create increasing-array.cpp

---
+++ increasing-array/increasing-array.cpp
@@ -0,0 +1,44 @@
+/* 
+ * CSES: Increasing Array
+ * Status: READY
+ * Time: 0.09 s
+ * Characters: 348
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int lngMax;
+    int usrInp;
+    int lstNumber = 0;
+    long int rslt = 0;
+ 
+    cin >> lngMax;
+    vector <int> nmbrs;
+ 
+    while (cin >> usrInp) {
+        nmbrs.push_back(usrInp);
+    }
+ 
+    for (int nmbr : nmbrs) {
+        
+        if (!lstNumber) {
+            lstNumber = nmbr;
+            continue;
+        }
+ 
+        if (lstNumber > nmbr) {
+            int diff = lstNumber - nmbr;
+            lstNumber = nmbr + diff;
+            rslt += diff;
+        } else {
+            lstNumber = nmbr;
+        }
+    }
+ 
+    cout << rslt;
+ 
+    return 0;
+}
